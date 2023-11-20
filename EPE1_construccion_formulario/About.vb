0edc9801eeea4809cdd13677866b7014439b314b
Pedro <72775296+wotanCode@users.noreply.github.com>
2023-11-20 14:10:47 -0300

    add comments

--- EPE1_construccion_formulario/About.vb
+++ EPE1_construccion_formulario/About.vb
@@ -1,28 +1,34 @@
 ﻿Public Class About
+    ' Este método se ejecuta cuando se hace clic en la opción del menú "GithubToolStripMenuItem".
     Private Sub GithubToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GithubToolStripMenuItem.Click
+        ' Se crea una nueva instancia de ProcessStartInfo para iniciar un proceso que abre la URL del perfil de GitHub.
         Dim psi As New ProcessStartInfo("cmd", "/c start https://www.github.com/wotanCode")
-        psi.CreateNoWindow = True
-        psi.UseShellExecute = False
-        psi.RedirectStandardOutput = True
-        Dim process As New Process() With {.StartInfo = psi}
-        process.Start()
+        psi.CreateNoWindow = True ' No se crea una ventana de consola.
+        psi.UseShellExecute = False ' No se utiliza el shell del sistema operativo.
+        psi.RedirectStandardOutput = True ' La salida estándar se redirige para su posterior lectura.
+        Dim process As New Process() With {.StartInfo = psi} ' Se crea un nuevo proceso con la información de inicio especificada.
+        process.Start() ' Se inicia el proceso para abrir la URL.
     End Sub
 
+    ' Este método se ejecuta cuando se hace clic en la opción del menú "LinkedinToolStripMenuItem".
     Private Sub LinkedinToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LinkedinToolStripMenuItem.Click
+        ' Se crea una nueva instancia de ProcessStartInfo para iniciar un proceso que abre la URL del perfil de LinkedIn.
         Dim psi As New ProcessStartInfo("cmd", "/c start https://www.linkedin.com/in/pedro-yanez/")
-        psi.CreateNoWindow = True
-        psi.UseShellExecute = False
-        psi.RedirectStandardOutput = True
-        Dim process As New Process() With {.StartInfo = psi}
-        process.Start()
+        psi.CreateNoWindow = True ' No se crea una ventana de consola.
+        psi.UseShellExecute = False ' No se utiliza el shell del sistema operativo.
+        psi.RedirectStandardOutput = True ' La salida estándar se redirige para su posterior lectura.
+        Dim process As New Process() With {.StartInfo = psi} ' Se crea un nuevo proceso con la información de inicio especificada.
+        process.Start() ' Se inicia el proceso para abrir la URL.
     End Sub
 
+    ' Este método se ejecuta cuando se hace clic en la opción del menú "InstagramToolStripMenuItem".
     Private Sub InstagramToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InstagramToolStripMenuItem.Click
+        ' Se crea una nueva instancia de ProcessStartInfo para iniciar un proceso que abre la URL del perfil de Instagram.
         Dim psi As New ProcessStartInfo("cmd", "/c start https://www.instagram.com/pedroelhumano")
-        psi.CreateNoWindow = True
-        psi.UseShellExecute = False
-        psi.RedirectStandardOutput = True
-        Dim process As New Process() With {.StartInfo = psi}
-        process.Start()
+        psi.CreateNoWindow = True ' No se crea una ventana de consola.
+        psi.UseShellExecute = False ' No se utiliza el shell del sistema operativo.
+        psi.RedirectStandardOutput = True ' La salida estándar se redirige para su posterior lectura.
+        Dim process As New Process() With {.StartInfo = psi} ' Se crea un nuevo proceso con la información de inicio especificada.
+        process.Start() ' Se inicia el proceso para abrir la URL.
     End Sub
 End Class
--- EPE1_construccion_formulario/WelcomeForm.vb
+++ EPE1_construccion_formulario/WelcomeForm.vb
@@ -1,10 +1,13 @@
 ﻿Public Class WelcomeForm
+
+    'Este método se ejecuta cuando se hace clic en el botón "CloseButton".
     Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
         Close()
     End Sub
 
+    'Este método se ejecuta cuando se selecciona la opción del menú "GithubToolStripMenuItem". Abre el navegador y se dirige al repositorio
     Private Sub GithubToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GithubToolStripMenuItem.Click
-        Dim psi As New ProcessStartInfo("cmd", "/c start https://www.github.com/wotanCode")
+        Dim psi As New ProcessStartInfo("cmd", "/c start https://github.com/wotanCode/VisualBasic.net-basic-practices/tree/main/EPE1_construccion_formulario")
         psi.CreateNoWindow = True
         psi.UseShellExecute = False
         psi.RedirectStandardOutput = True
@@ -12,21 +15,25 @@
         process.Start()
     End Sub
 
+    'Este método se ejecuta cuando se hace clic en el botón "ButtonAccessCalculator". Abre el formulario de la calculadora.
     Private Sub ButtonAccessCalculator_Click(sender As Object, e As EventArgs) Handles ButtonAccessCalculator.Click
         Dim calculatorForm As New CalculatorForm()
         calculatorForm.Show()
     End Sub
 
+    'Este método se ejecuta cuando se hace clic en el botón "ButtonAccessCalendary". Abre el formulario del calendario.
     Private Sub ButtonAccessCalendary_Click(sender As Object, e As EventArgs) Handles ButtonAccessCalendary.Click
         Dim calendaryForm As New CalendaryForm()
         calendaryForm.Show()
     End Sub
 
+    'Este método se ejecuta cuando se hace clic en el botón "ButtonAccessLista". Accede al formulario de la lista de tareas
     Private Sub ButtonAccessLista_Click(sender As Object, e As EventArgs) Handles ButtonAccessLista.Click
         Dim listForm As New ListForm()
         listForm.Show()
     End Sub
 
+    'Este método se ejecuta cuando se selecciona la opción del menú "AcercaDelAutorToolStripMenuItem". Accede al formulario donde se muestra información acerca del desarrollador
     Private Sub AcercaDelAutorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AcercaDelAutorToolStripMenuItem.Click
         Dim about As New About()
         about.Show()
